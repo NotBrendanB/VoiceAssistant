bb6c2db545d3c3f8524870666edd60c49982a8cc
Brendan Barnes <brendanbarnessoccer@gmail.com>
2026-07-22 14:51:06 -0400

    Finish File

--- AIConnection.py
+++ AIConnection.py
@@ -1,22 +1,21 @@
 import os
 
-print("YOUR CURRENT KEY IS:", os.environ.get("GROQ_API_KEY"))
-
 from groq import Groq
 
-client = Groq(api_key=os.environ.get("GROQ_API_KEY"),
-)
+def AI_response(userPrompt):
+    client = Groq(api_key=os.environ.get("GROQ_API_KEY"),
+    )
 
-chat_completion = client.chat.completions.create(
-    messages = [
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models", 
-        }
-    ],
-    model = "llama-3.3-70b-versatile",
-)
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {
+                "role": "user",
+                "content": userPrompt, 
+            }
+        ],
+        model = "llama-3.3-70b-versatile",
+    )
 
-print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
 
 # the idea is to keep the last 5 back and forth chats saved to a file, and use that for conversation history
